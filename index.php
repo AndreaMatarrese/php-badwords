2d7a0e36ea4c110e678e283ee76686767b4758dd
AndreaMatarrese <matarreseandrea47@gmail.com>
2022-12-13 15:41:30 +0100

    Exercise

--- index.php
+++ index.php
@@ -4,9 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Php badwords</title>
 </head>
 <body>
-    
+    <?php
+    $original_text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum accusantium commodi quo iste eligendi rem impedit quae modi corporis aut labore dolore, amet odio nisi voluptates pariatur voluptatum eum quidem?';
+    $censure = $_GET['censure'];
+    $substitute = '***';
+    $corrected_text = str_ireplace($censure,$substitute,$original_text);
+    ?>
+        <h1 class="text-center">Attenzione alle parole offensive!</h1>
+        <p class="text-center">Scegli nel testo le parole che ritieni offensive</p>
+    <div class="main row row-cols-2 justify-content-center">
+        <div class="original col-5">
+            <h3>Testo originale</h3>
+            <p>
+                <?= $original_text?>
+            </p>
+            <?= 'Il testo è lungo' . ' ' . strlen($original_text) . ' ' . 'caratteri'?>
+            <form action="" method="get">
+                <label for="censure">Inserisci la parola che ritieni offensiva</label>
+                <input type="text" name="censure" id="censure">
+                <button class="btn btn-primary">Sostituisci</button>
+            </form>
+        </div>
+        <div class="corrected col-5">
+            <h3>Testo corretto</h3>
+            <p>
+                <?= $corrected_text ?>
+            </p>
+            <?= 'Il testo è lungo' . ' ' . strlen($corrected_text) . ' ' . 'caratteri'?>
+        </div>
+    </div>
 </body>
 </html>
